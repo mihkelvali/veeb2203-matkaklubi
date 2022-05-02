6bec8bb302cea5609031ed45823be4bfd725e5cc
Mihkel Väli <mihkel.vali@barking.ee>
2022-05-02 18:29:34 +0300

    matkaklubi senini tehtud koodi importimine

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function renderNews(request, response) {
+  response.render('pages/news')
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/contact', (req, res) => res.render('pages/contact'))
+  .get('/news', renderNews)
   .listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
